1efb0b510f39b38c7ec7809f2d2a1fd7be8c5d9c
Ian Shimmings <ian.shimmings@hotmail.com>
2024-12-04 13:40:57 +0000

    Automatically create https proxy

--- Btms.Backend.Test/Config/ApiOptionsTest.cs
+++ Btms.Backend.Test/Config/ApiOptionsTest.cs
@@ -6,43 +6,37 @@ namespace Btms.Backend.Test.Config;
 
 public class ApiOptionsTest
 {
-    private IValidateOptions<ApiOptions> validator = new ApiOptions.Validator();
+    private readonly IValidateOptions<ApiOptions> validator = new ApiOptions.Validator();
     
     [Fact]
     public void ShouldSucceedIfNoCdsProxy()
     {
-        var c = new ApiOptions() { };
+        var c = new ApiOptions();
 
         validator.Validate("", c).Should().Be(ValidateOptionsResult.Success);
     }
     
     [Fact]
-    public void ShouldSucceedIfCdsProxyAndHttpsProxy()
-    {
-        var c = new ApiOptions() { CdpHttpsProxy = "https://aaa", HttpsProxy = "aaa", };
-    
-        validator.Validate("", c).Should().Be(ValidateOptionsResult.Success);
-    }
-    
-    [Fact]
-    public void ShouldFailIfCdsProxyAndNotHttpsProxy()
-    {
-        var c = new ApiOptions() { CdpHttpsProxy = "https://aaa" };
-    
-        validator.Validate("", c).Failed.Should().BeTrue();
-    }
-    
-    [Fact]
     public void ShouldFailIfCdsProxyDoesntHaveProtocol()
     {
-        var c = new ApiOptions() { CdpHttpsProxy = "aaa", HttpsProxy = "aaa", };
+        var c = new ApiOptions() { CdpHttpsProxy = "aaa" };
+        
         validator.Validate("", c).Failed.Should().BeTrue();
     }
     
     [Fact]
-    public void ShouldFailIfHttpsProxyHasProtocol()
+    public void ShouldSetHttpsProxyIfCdsProxyPresent()
     {
-        var c = new ApiOptions() { CdpHttpsProxy = "https://aaa", HttpsProxy = "https://aaa", };
-        validator.Validate("", c).Failed.Should().BeTrue();
+        var c = new ApiOptions { CdpHttpsProxy = "https://aaa" };
+
+        c.HttpsProxy.Should().Be("aaa");
+    }
+    
+    [Fact]
+    public void ShouldNotSetHttpsProxyIfCdsProxyPresent()
+    {
+        var c = new ApiOptions();
+
+        c.HttpsProxy.Should().BeNull();
     }
 }
