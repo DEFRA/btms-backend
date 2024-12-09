5024f6623bcd19e7a3e9c5b297cc3173faca1152
Craig Edmunds <craig.edmunds@gmail.com>
2024-12-09 15:07:29 +0000

    CDMS-179 adds lastMonthImportNotificationsByTypeAndStatus back in

--- Btms.Backend/Endpoints/AnalyticsEndpoints.cs
+++ Btms.Backend/Endpoints/AnalyticsEndpoints.cs
@@ -77,6 +77,7 @@ public static class AnalyticsEndpoints
             lastMonthMovementsByItemCount,
             lastMonthImportNotificationsByCommodityCount,
             lastMonthMovementsByUniqueDocumentReferenceCount,
+            lastMonthImportNotificationsByTypeAndStatus,
             lastMonthUniqueDocumentReferenceByMovementCount
         });
     }
