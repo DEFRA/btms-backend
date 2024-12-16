2bfae1832a6e30fc3be5d0c96e277cbadd72202a
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2024-12-16 07:43:50 +0000

    reverted the decision finders back

--- Btms.Business/Services/Decisions/Finders/ChedPDecisionFinder.cs
+++ Btms.Business/Services/Decisions/Finders/ChedPDecisionFinder.cs
@@ -6,33 +6,6 @@ public class ChedPDecisionFinder : IDecisionFinder
 {
     public DecisionFinderResult FindDecision(ImportNotification notification)
     {
-        var consignmentAcceptable = notification.PartTwo?.Decision?.ConsignmentAcceptable;
-        if (consignmentAcceptable is true)
-        {
-            return notification.PartTwo?.Decision?.DecisionEnum switch
-            {
-                DecisionDecisionEnum.AcceptableForTranshipment or 
-                    DecisionDecisionEnum.AcceptableForTransit or 
-                    DecisionDecisionEnum.AcceptableForSpecificWarehouse => new DecisionFinderResult(DecisionCode.E03),
-                DecisionDecisionEnum.AcceptableForInternalMarket => new DecisionFinderResult(DecisionCode.C03),
-                DecisionDecisionEnum.AcceptableIfChanneled => new DecisionFinderResult(DecisionCode.C06), 
-                _ => throw new ArgumentOutOfRangeException(nameof(notification),
-                    "Decision type unexpected for cheda when consignment acceptable")
-            };
-        }
-
-        if (consignmentAcceptable is false)
-        {
-            return notification.PartTwo?.Decision?.NotAcceptableAction switch
-            {
-                DecisionNotAcceptableActionEnum.Destruction  => new DecisionFinderResult(DecisionCode.N02),
-                DecisionNotAcceptableActionEnum.Reexport => new DecisionFinderResult(DecisionCode.N04),
-                DecisionNotAcceptableActionEnum.Transformation => new DecisionFinderResult(DecisionCode.N03),
-                DecisionNotAcceptableActionEnum.Other => new DecisionFinderResult(DecisionCode.N07),
-                _ => throw new ArgumentOutOfRangeException(nameof(notification),
-                    "Decision type unexpected for cheda when consignment acceptable")
-            };
-        }
-        return new DecisionFinderResult(DecisionCode.C03);
+        throw new NotImplementedException();
     }
 }
