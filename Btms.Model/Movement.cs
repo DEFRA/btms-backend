bd17501cd714eeb38e2ed509c9344fe5c2569670
Tris <tristan.sellick-taylor@esynergy.co.uk>
2024-12-20 15:49:04 +0000

    Implemented clearing down removed links from notifications

--- Btms.Model/Movement.cs
+++ Btms.Model/Movement.cs
@@ -68,7 +68,6 @@ public class Movement : IMongoIdentifiable, IDataEntity, IAuditable
 
 
     [BsonElement("_matchReferences")]
-    [ChangeSetIgnore]
     public List<string> _MatchReferences
     {
         get
