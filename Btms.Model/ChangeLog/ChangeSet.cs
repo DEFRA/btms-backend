fff69fa57edbd67b1cfd0882cd5667aa44596f98
Tris <tristan.sellick-taylor@esynergy.co.uk>
2024-12-20 15:49:04 +0000

    Implemented clearing down removed links from notifications

--- Btms.Model/ChangeLog/ChangeSet.cs
+++ Btms.Model/ChangeLog/ChangeSet.cs
@@ -6,9 +6,9 @@ using Json.Patch;
 
 namespace Btms.Model.ChangeLog;
 
-public class ChangeSet(JsonPatch jsonPatch)
+public class ChangeSet(JsonPatch jsonPatch, JsonNode jsonNodePrevious)
 {
-    private static JsonSerializerOptions _jsonOptions = new()
+    private static readonly JsonSerializerOptions _jsonOptions = new()
     {
         TypeInfoResolver = new ChangeSetTypeInfoResolver(),
         PropertyNameCaseInsensitive = true,
@@ -17,15 +17,17 @@ public class ChangeSet(JsonPatch jsonPatch)
 
     public JsonPatch JsonPatch { get; } = jsonPatch;
 
+    public JsonNode Previous { get; } = jsonNodePrevious;
+
     public static ChangeSet CreateChangeSet<T>(T current, T previous)
     {
         var previousNode = JsonNode.Parse(previous.ToJsonString(_jsonOptions));
         var currentNode = JsonNode.Parse(current.ToJsonString(_jsonOptions));
         var diff = previousNode.CreatePatch(currentNode);
-
+        
         //exclude fields from patch, like _ts, audit entries etc
         var operations = diff.Operations.Where(x => !x.Path.ToString().Contains("_ts"));
 
-        return new ChangeSet(new JsonPatch(operations));
+        return new ChangeSet(new JsonPatch(operations), previousNode!);
     }
 }
