cdf93621b48b15d8b4973d7c0913f9f89c4ef68b
Lim Sim <lim.sim@equalexperts.com>
2024-12-28 00:31:47 +0000

    * Added scenario to test unmatched CHEDs. * Updated README with instructions on how to set up test data. * Made a couple of fixes.

---
+++ TestDataGenerator/Scenarios/AllChedsNoMatchScenarioGenerator.cs
@@ -0,0 +1,48 @@
+using Btms.Types.Ipaffs;
+using Microsoft.Extensions.Logging;
+
+namespace TestDataGenerator.Scenarios;
+
+public class AllChedsNoMatchScenarioGenerator(ILogger<AllChedsNoMatchScenarioGenerator> logger) : ScenarioGenerator
+{
+    public override GeneratorResult Generate(int scenario, int item, DateTime entryDate, ScenarioConfig config)
+    {
+        var chedANotification = GetNotificationBuilder("cheda-one-commodity")
+            .WithCreationDate(entryDate)
+            .WithRandomArrivalDateTime(config.ArrivalDateRange)
+            .WithReferenceNumber(ImportNotificationTypeEnum.Cveda, scenario, entryDate, item)
+            .ValidateAndBuild();
+
+        logger.LogInformation("Created {NotificationReferenceNumber}", 
+            chedANotification.ReferenceNumber);
+        
+        var chedPNotification = GetNotificationBuilder("chedp-one-commodity")
+            .WithCreationDate(entryDate)
+            .WithRandomArrivalDateTime(config.ArrivalDateRange)
+            .WithReferenceNumber(ImportNotificationTypeEnum.Cvedp, scenario, entryDate, item)
+            .ValidateAndBuild();
+        
+        logger.LogInformation("Created {NotificationReferenceNumber}", 
+            chedPNotification.ReferenceNumber);
+        
+        var chedDNotification = GetNotificationBuilder("chedd-one-commodity")
+            .WithCreationDate(entryDate)
+            .WithRandomArrivalDateTime(config.ArrivalDateRange)
+            .WithReferenceNumber(ImportNotificationTypeEnum.Ced, scenario, entryDate, item)
+            .ValidateAndBuild();
+        
+        logger.LogInformation("Created {NotificationReferenceNumber}", 
+            chedDNotification.ReferenceNumber);
+        
+        var chedPPNotification = GetNotificationBuilder("chedpp-multiple-commodity")
+            .WithCreationDate(entryDate)
+            .WithRandomArrivalDateTime(config.ArrivalDateRange)
+            .WithReferenceNumber(ImportNotificationTypeEnum.Chedpp, scenario, entryDate, item)
+            .ValidateAndBuild();
+        
+        logger.LogInformation("Created {NotificationReferenceNumber}", 
+            chedPPNotification.ReferenceNumber);
+
+        return new GeneratorResult([chedANotification, chedPNotification, chedDNotification, chedPPNotification]);
+    }
+}
