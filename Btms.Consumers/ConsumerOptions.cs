f38c4ad43e0645f31ceb861bce77d58aafeab5b7
t11omas <t11omas1983@live.co.uk>
2025-01-08 06:37:44 +0000

    Added options to be able to control the consumer retry count (#49)
    Co-authored-by: Thomas Anderson <thomas.anderson@esynergy.co.uk>

--- Btms.Consumers/ConsumerOptions.cs
+++ Btms.Consumers/ConsumerOptions.cs
@@ -5,9 +5,11 @@ public class ConsumerOptions
     public const string SectionName = nameof(ConsumerOptions);
 
     public bool EnableBlockingPublish { get; set; } = false;
-    public int InMemoryNotifications { get; set; } = 2;
+    public int InMemoryNotifications { get; set; } = 20;
     public int InMemoryGmrs { get; set; } = 2;
-    public int InMemoryClearanceRequests { get; set; } = 2;
+    public int InMemoryClearanceRequests { get; set; } = 20;
     public int InMemoryDecisions { get; set; } = 2;
-    
+
+    public int ErrorRetries { get; set; } = 10;
+
 }
