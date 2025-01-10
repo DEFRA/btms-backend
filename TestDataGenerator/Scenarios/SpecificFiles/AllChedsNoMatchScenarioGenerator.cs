e007bd70098ecaf87075d4bcf0d52cd70594e0a4
Lim Sim <lim.sim@equalexperts.com>
2025-01-10 15:20:32 +0000

    Fix merge conflicts.

--- TestDataGenerator/Scenarios/SpecificFiles/AllChedsNoMatchScenarioGenerator.cs
+++ TestDataGenerator/Scenarios/SpecificFiles/AllChedsNoMatchScenarioGenerator.cs
@@ -47,43 +47,5 @@ public class AllChedsNoMatchScenarioGenerator(IServiceProvider sp, ILogger<AllCh
 
 
         return new GeneratorResult([chedAMessage, chedPMessage, chedDMessage, chedPPMessage]);
-
-        // var chedANotification = GetNotificationBuilder("cheda-one-commodity")
-        //     .WithCreationDate(entryDate)
-        //     .WithRandomArrivalDateTime(config.ArrivalDateRange)
-        //     .WithReferenceNumber(ImportNotificationTypeEnum.Cveda, scenario, entryDate, item)
-        //     .ValidateAndBuild();
-        //
-        // logger.LogInformation("Created {NotificationReferenceNumber}", 
-        //     chedANotification.ReferenceNumber);
-        //
-        // var chedPNotification = GetNotificationBuilder("chedp-one-commodity")
-        //     .WithCreationDate(entryDate)
-        //     .WithRandomArrivalDateTime(config.ArrivalDateRange)
-        //     .WithReferenceNumber(ImportNotificationTypeEnum.Cvedp, scenario, entryDate, item)
-        //     .ValidateAndBuild();
-        //
-        // logger.LogInformation("Created {NotificationReferenceNumber}", 
-        //     chedPNotification.ReferenceNumber);
-        //
-        // var chedDNotification = GetNotificationBuilder("chedd-one-commodity")
-        //     .WithCreationDate(entryDate)
-        //     .WithRandomArrivalDateTime(config.ArrivalDateRange)
-        //     .WithReferenceNumber(ImportNotificationTypeEnum.Ced, scenario, entryDate, item)
-        //     .ValidateAndBuild();
-        //
-        // logger.LogInformation("Created {NotificationReferenceNumber}", 
-        //     chedDNotification.ReferenceNumber);
-        //
-        // var chedPPNotification = GetNotificationBuilder("chedpp-multiple-commodity")
-        //     .WithCreationDate(entryDate)
-        //     .WithRandomArrivalDateTime(config.ArrivalDateRange)
-        //     .WithReferenceNumber(ImportNotificationTypeEnum.Chedpp, scenario, entryDate, item)
-        //     .ValidateAndBuild();
-        //
-        // logger.LogInformation("Created {NotificationReferenceNumber}", 
-        //     chedPPNotification.ReferenceNumber);
-        //
-        // return new GeneratorResult([chedANotification, chedPNotification, chedDNotification, chedPPNotification]);
     }
 }
