1aa3f3556c06bfe27dafb5fe65ffb143c93cf46e
Lim Sim <lim.sim@equalexperts.com>
2025-01-15 21:26:18 +0000

    Refactor test to simply how they work.
    Creates seperate ChedPH01DecisionGenerator
    
    Adds data driven test
    
    Adds CheckCode method
    
    Implements MultipleScenarioGeneratorBaseTest and continues to refactor decision scenarios

---
+++ Btms.Backend.IntegrationTests/DecisionTests/BasicDecisionCodeTests.cs
@@ -0,0 +1,84 @@
+using Btms.Model;
+using FluentAssertions;
+using TestDataGenerator.Scenarios.SpecificFiles;
+using TestGenerator.IntegrationTesting.Backend;
+using TestGenerator.IntegrationTesting.Backend.Fixtures;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Btms.Backend.IntegrationTests.DecisionTests;
+
+[Trait("Category", "Integration")]
+public class BasicDecisionCodeTests
+{
+    public class SingleChedH01Tests(ITestOutputHelper output)
+        : ScenarioGeneratorBaseTest<AllChedsH01DecisionGenerator>(output)
+    {
+        [Fact]
+        public void SingleChed_ShouldHaveH02DecisionCode()
+        {
+            CheckDecisionCode("H01", Client);
+        }
+    }
+    
+    public class SingleChedH02Tests(ITestOutputHelper output)
+        : ScenarioGeneratorBaseTest<AllChedsH02DecisionGenerator>(output)
+    {
+        [Fact]
+        public void SingleChed_ShouldHaveH02DecisionCode()
+        {
+            CheckDecisionCode("H02", Client);
+        }
+    }
+    
+    [Trait("Category", "Integration")]
+    public class SingleChedDecisionTests(ITestOutputHelper output)
+        : ScenarioGeneratorBaseTest<AllChedsC03DecisionGenerator>(output)
+    {
+        [Fact]
+        public void SingleChed_ShouldHaveC03DecisionCode()
+        {
+            CheckDecisionCode("C03", Client);
+        }
+    }
+    
+    [Trait("Category", "Integration")]
+    public class MultiChedDecisionTest(ITestOutputHelper output)
+        : ScenarioGeneratorBaseTest<MultiChedPMatchScenarioGenerator>(output)
+    {
+        [Fact]
+        public void MultiChed_ShouldHaveH02DecisionCode()
+        {
+            string decisionCode = "";
+            var expectedDecision = "H02";
+            Client.AsJsonApiClient().Get("api/movements").GetResourceObjects<Movement>().Single().Decisions
+                .OrderBy(x => x.ServiceHeader?.ServiceCalled).Last().Items!
+                .All(i =>
+                {
+                    decisionCode = i.Checks!.First().DecisionCode!;
+
+                    return decisionCode.Equals(expectedDecision);
+                }).Should().BeTrue($"Expected {expectedDecision}. Actually {{0}}", decisionCode);
+        }
+    }
+    
+    private static void CheckDecisionCode(string expectedDecisionCode, BtmsClient client)
+    {
+        string decisionCode;
+        string mrn;
+
+        client.AsJsonApiClient().Get("api/movements").GetResourceObjects<Movement>().Should().AllSatisfy(m =>
+        {
+            mrn = m.EntryReference;
+            m.Decisions.OrderBy(x => x.ServiceHeader?.ServiceCalled).Last().Items.Should().AllSatisfy(i =>
+            {
+                i.Checks.Should().AllSatisfy(c =>
+                {
+                    decisionCode = c.DecisionCode!;
+                    decisionCode.Should().Be(expectedDecisionCode,
+                        $"Expected {expectedDecisionCode}. Actually {{0}}. MRN: {{1}}", decisionCode, mrn);
+                });
+            });
+        });
+    }
+}
