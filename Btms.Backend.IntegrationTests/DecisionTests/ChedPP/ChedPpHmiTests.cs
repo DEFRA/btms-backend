c2159a010c7169dfad5f0a8196e60f2f067ec1f2
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-01-23 13:33:31 +0000

    fixed up some test data

--- Btms.Backend.IntegrationTests/DecisionTests/ChedPP/ChedPpHmiTests.cs
+++ Btms.Backend.IntegrationTests/DecisionTests/ChedPP/ChedPpHmiTests.cs
@@ -1,6 +1,3 @@
-using Btms.Model.Auditing;
-using Btms.Model.Cds;
-using Btms.Types.Ipaffs;
 using FluentAssertions;
 using TestDataGenerator.Scenarios.SpecificFiles;
 using TestGenerator.IntegrationTesting.Backend;
@@ -20,8 +17,7 @@ public class ChedPpHmiTests(ITestOutputHelper output) : MultipleScenarioGenerato
     [InlineData(typeof(ChedPpHmiDecisionTestsScenarioGenerator), "24GBD69TMXZ2TYCAR9", "N02")]
     [InlineData(typeof(ChedPpHmiDecisionTestsScenarioGenerator), "24GBD69TMXZ2TYCAR8", "H01")]
     [InlineData(typeof(ChedPpHmiDecisionTestsScenarioGenerator), "24GBD69TMXZ2TYCAR7", "H02")]
-    ////[InlineData(typeof(ChedPpPhsiDecisionTestsScenarioGenerator), "24GBDB6P6GWPVLKAR7", "C03")] put scenario in once HMI has been implemented 
-    public void DecisionShouldHaveCorrectDecisionCodeForSingleNotification(Type generatorType, string mrn, string decisionCode)
+     public void DecisionShouldHaveCorrectDecisionCodeForSingleNotification(Type generatorType, string mrn, string decisionCode)
     {
         base.TestOutputHelper.WriteLine("Generator : {0}, Decision Code : {1}", generatorType!.FullName, decisionCode);
         EnsureEnvironmentInitialised(generatorType);
