63558561679e8378ee16fb52bf2c7ee01f21cc9a
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-01-28 06:57:11 +0000

    deleted test

--- Btms.Backend.IntegrationTests/DecisionTests/ChedPP/ChedPpHmiTests.cs
+++ Btms.Backend.IntegrationTests/DecisionTests/ChedPP/ChedPpHmiTests.cs
@@ -17,29 +17,10 @@ public class ChedPpHmiTests(ITestOutputHelper output) : MultipleScenarioGenerato
     [InlineData(typeof(ChedPpHmiDecisionTestsScenarioGenerator), "24GBD69TMXZ2TYCAR9", "N02")]
     [InlineData(typeof(ChedPpHmiDecisionTestsScenarioGenerator), "24GBD69TMXZ2TYCAR8", "H01")]
     [InlineData(typeof(ChedPpHmiDecisionTestsScenarioGenerator), "24GBD69TMXZ2TYCAR7", "H02")]
-     public void DecisionShouldHaveCorrectDecisionCodeForSingleNotification(Type generatorType, string mrn, string decisionCode)
-    {
-        base.TestOutputHelper.WriteLine("Generator : {0}, Decision Code : {1}", generatorType!.FullName, decisionCode);
-        EnsureEnvironmentInitialised(generatorType);
 
-        var movement = Client
-            .GetMovementByMrn(mrn);
-
-        var lastDecision = movement.Decisions.OrderByDescending(x => x.ServiceHeader?.ServiceCalled).First();
-
-
-        foreach (var item in lastDecision.Items!)
-        {
-            foreach (var itemCheck in item.Checks!)
-            {
-                itemCheck.DecisionCode.Should().Be(decisionCode);
-            }
-        }
-    }
-
-    [Theory]
+    //Movement With Different Ched Types, that caused a finder exceptions
     [InlineData(typeof(Mrn24GBDY6XFF66H0XAR1ScenarioGenerator), "24GBDY6XFF66H0XAR1", "C03")]
-    public void DecisionShouldHaveCorrectDecisionCodeForSingleNotification1(Type generatorType, string mrn, string decisionCode)
+public void DecisionShouldHaveCorrectDecisionCodeForSingleNotification(Type generatorType, string mrn, string decisionCode)
     {
         base.TestOutputHelper.WriteLine("Generator : {0}, Decision Code : {1}", generatorType!.FullName, decisionCode);
         EnsureEnvironmentInitialised(generatorType);
