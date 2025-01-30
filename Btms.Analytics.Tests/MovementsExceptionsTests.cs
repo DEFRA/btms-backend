bc02363a132f980b60aaa26a1fe591e13e4f9f9d
Craig Edmunds <craig.edmunds@gmail.com>
2025-01-30 09:03:19 +0000

    Feature/cdms-215 adds additional exception examples for permutations of DecisionStatus Status & Segment (#96)
    * CDMS-215 adds additional exception examples for permutations of DecisionStatus Status & Segment
    
    * Fixes sonarqube issue

--- Btms.Analytics.Tests/MovementsExceptionsTests.cs
+++ Btms.Analytics.Tests/MovementsExceptionsTests.cs
@@ -7,34 +7,70 @@ using Btms.Analytics.Tests.Fixtures;
 using Btms.Analytics.Tests.Helpers;
 using TestDataGenerator.Config;
 using TestDataGenerator.Scenarios;
+using TestDataGenerator.Scenarios.SpecificFiles;
 using TestGenerator.IntegrationTesting.Backend;
 using TestGenerator.IntegrationTesting.Backend.Extensions;
 
 namespace Btms.Analytics.Tests;
 
-public class MovementsExceptionsTests(ITestOutputHelper output)
-    : ScenarioGeneratorBaseTest<CrNoMatchSingleItemWithDecisionScenarioGenerator>(output)
+[Trait("Category", "Integration")]
+public class MovementsExceptionsTests(ITestOutputHelper output) : MultipleScenarioGeneratorBaseTest(output)
 {
-    
-    [Fact] //(Skip="Change to an individiual scenario test that generates an exception. Possibly a seperate test for each exception type")]
-    public async Task WhenCalled_ReturnExpectedAggregation()
+    static readonly List<object[]> scenarios = [
+        [typeof(Mrn24Gbd46Kpsvz3Dfar2ScenarioGenerator), "HasChedppChecks FeatureMissing : Cdms205Ac5"],
+        [typeof(CrNoMatchSingleItemWithDecisionScenarioGenerator), "Alvs has match decisions but no Btms links"]
+    ];
+
+    public static IEnumerable<object[]> Scenarios()
     {
-        TestOutputHelper.WriteLine("Querying for aggregated data");
+        foreach (var scenario in scenarios)
+        {
+            yield return [scenario[0]];
+        }
+    }
+
+    public static IEnumerable<object[]> ScenariosWithException()
+    {
+        foreach (var scenario in scenarios)
+        {
+            yield return [scenario[0], scenario[1]];
+        }
+    }
+
+    private async Task<SingleSeriesDataset> EnsureEnvironmentInitialisedAndGetExceptions(Type generatorType)
+    {
+        EnsureEnvironmentInitialised(generatorType);
         
         var result = await Client
             .GetAnalyticsDashboard(["movementsExceptions"],
                 dateFrom: DateTime.MinValue, dateTo: DateTime.MaxValue);
-        
+         
         var chart = await result
             .AnalyticsChartAs<SingleSeriesDataset>("movementsExceptions")!;
-        
-        chart.Values.Count
-            .Should().BeGreaterThan(1);
-        
-        chart.Values
-            .Single(v => v.Key == "Alvs has match decisions but no Btms links")
-            .Value
-            .Should().Be(1);
-        
+
+        return chart;
     }
+    
+    [Theory]
+    [MemberData(nameof(Scenarios))]
+    public async Task ShouldHaveAnException(Type generatorType)
+    {
+        var chart = await EnsureEnvironmentInitialisedAndGetExceptions(generatorType);
+         
+         chart.Values.Count
+             .Should().BeGreaterThan(1);
+    }
+    
+    
+    [Theory]
+    [MemberData(nameof(ScenariosWithException))]
+    public async Task ShouldHaveCorrectException(Type generatorType, string expectedException)
+    {
+        var chart = await EnsureEnvironmentInitialisedAndGetExceptions(generatorType);
+
+         chart.Values
+             .Single(v => v.Key == expectedException)
+             .Value
+             .Should().Be(1);
+         }
 }
