02d70dfbe6fdc3e0c0648e7e2e46b84911771855
Craig Edmunds <craig.edmunds@gmail.com>
2025-01-31 14:48:33 +0000

    Fix format issues

--- TestDataGenerator/Scenarios/SpecificFiles/Mrn24Gbde8Olvkzxsyar1ReorderingScenarioGenerator.cs
+++ TestDataGenerator/Scenarios/SpecificFiles/Mrn24Gbde8Olvkzxsyar1ReorderingScenarioGenerator.cs
@@ -19,13 +19,6 @@ public class Mrn24Gbde8Olvkzxsyar1ImportNotificationsAtEndScenarioGenerator(ISer
                 return x!.CreatedDate().CompareTo(y!.CreatedDate());
         }
     }
-    
-    // protected override List<(string filePath, IBaseBuilder builder)> ModifyBuilders(List<(string filePath, IBaseBuilder builder)> builders)
-    // {
-    //     builders.Order(new PutImportNotificationsAtEndComparer());
-    //     
-    //     return builders;
-    // }
 
     protected override List<object> ModifyMessages(List<object> messages)
     {
@@ -33,28 +26,4 @@ public class Mrn24Gbde8Olvkzxsyar1ImportNotificationsAtEndScenarioGenerator(ISer
             .Order(new PutImportNotificationsAtEndComparer())
             .ToList();
     }
-
-    // public override GeneratorResult Generate(int scenario, int item, DateTime entryDate, ScenarioConfig config)
-    // {
-    //     var builders =  GetBuilders("Mrn-24GBDEJ9V2OD0BHAR0").GetAwaiter().GetResult();
-    //     
-    //     logger.LogInformation("Created {Builders} Builders", 
-    //         builders.Count);
-    //     
-    //     // Set the manual action of the builders to "Y"
-    //     foreach (var builderItem in builders
-    //                  .Where(b => b.builder is FinalisationBuilder))
-    //     {
-    //         ((FinalisationBuilder)builderItem.builder)
-    //             .WithManualAction(true);
-    //     }
-    //     
-    //     var messages = builders
-    //         .Select(b => b.builder)
-    //         .ToArray()
-    //         .BuildAll()
-    //         .ToArray();
-    //     
-    //     return new GeneratorResult(messages);
-    // }
 }
