a0054bfda3f779c81ea5208397c435df49083a13
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-01-31 09:18:07 +0000

    make sure blob ends with *.json

--- Btms.BlobService/BtmsBlobItem.cs
+++ Btms.BlobService/BtmsBlobItem.cs
@@ -1,9 +1,13 @@
+using System.Diagnostics;
+
 namespace Btms.BlobService;
 
+[DebuggerDisplay("{CreatedOn} - {Name}")]
 public class BtmsBlobItem : IBlobItem
 {
     public string Name { get; set; } = default!;
     public string NormalisedName { get; set; } = default!;
     public string Content { get; set; } = default!;
+    public DateTimeOffset? CreatedOn { get; set; }
 
 }
