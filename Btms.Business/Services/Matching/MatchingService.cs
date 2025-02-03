4338bc86c5470933db8761427b47b7c7c6ec4ed1
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-02-03 06:35:51 +0000

    refactor matching service

--- Btms.Business/Services/Matching/MatchingService.cs
+++ Btms.Business/Services/Matching/MatchingService.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Btms.Business.Extensions;
 using Btms.Model;
+using Btms.Model.Cds;
 
 namespace Btms.Business.Services.Matching;
 
@@ -23,35 +24,41 @@ public class MatchingService : IMatchingService
 
                 foreach (var documentGroup in groupedDocuments)
                 {
-                    Debug.Assert(documentGroup != null);
-                    Debug.Assert(movement.Id != null, "movement.Id != null");
-                    Debug.Assert(item.ItemNumber != null, "item.ItemNumber != null");
-
-                    if (MatchIdentifier.TryFromCds(documentGroup!, out var identifier))
-                    {
-                        var notification = matchingContext.Notifications.Find(x =>
-                            x._MatchReference == identifier.Identifier);
-
-                        if (notification is null)
-                        {
-                            matchingResult.AddDocumentNoMatch(movement.Id, item.ItemNumber.Value, documentGroup);
-                        }
-                        else
-                        {
-                            Debug.Assert(notification?.Id != null, "notification.Id != null");
-                            matchingResult.AddMatch(notification.Id, movement.Id, item.ItemNumber.Value, documentGroup);
-                        }
-
-
-                    }
-                    else
-                    {
-                        matchingResult.AddDocumentNoMatch(movement.Id, item.ItemNumber.Value, documentGroup);
-                    }
+                    ProcessDocument(matchingContext, documentGroup, movement, item, matchingResult);
                 }
             }
         }
 
         return Task.FromResult(matchingResult);
+    }
+
+    private static void ProcessDocument(MatchingContext matchingContext, string? documentGroup, Movement movement,
+        Items item, MatchingResult matchingResult)
+    {
+        Debug.Assert(documentGroup != null);
+        Debug.Assert(movement.Id != null, "movement.Id != null");
+        Debug.Assert(item.ItemNumber != null, "item.ItemNumber != null");
+
+        if (MatchIdentifier.TryFromCds(documentGroup!, out var identifier))
+        {
+            var notification = matchingContext.Notifications.Find(x =>
+                x._MatchReference == identifier.Identifier);
+
+            if (notification is null)
+            {
+                matchingResult.AddDocumentNoMatch(movement.Id, item.ItemNumber.Value, documentGroup);
+            }
+            else
+            {
+                Debug.Assert(notification?.Id != null, "notification.Id != null");
+                matchingResult.AddMatch(notification.Id, movement.Id, item.ItemNumber.Value, documentGroup);
+            }
+
+
+        }
+        else
+        {
+            matchingResult.AddDocumentNoMatch(movement.Id, item.ItemNumber.Value, documentGroup);
+        }
     }
 }
