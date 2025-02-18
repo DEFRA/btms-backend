5e42fe07c3c112e874c1112d00caa3899e6ed617
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-02-18 07:38:20 +0000

    formatted code

--- Btms.Backend.IntegrationTests/SensitiveDataTests.cs
+++ Btms.Backend.IntegrationTests/SensitiveDataTests.cs
@@ -8,56 +8,58 @@ using Xunit;
 
 namespace Btms.Backend.IntegrationTests;
 
-
 public class SensitiveDataTests
 {
     [Fact]
     public void WhenIncludeSensitiveData_RedactedShouldBeSameAsJson()
     {
-        var filePath = "../../../Fixtures/SmokeTest/IPAFFS/CHEDA/CHEDA_GB_2024_1041389-ee0e6fcf-52a4-45ea-8830-d4553ee70361.json";
+        var filePath =
+            "../../../Fixtures/SmokeTest/IPAFFS/CHEDA/CHEDA_GB_2024_1041389-ee0e6fcf-52a4-45ea-8830-d4553ee70361.json";
         var json =
             File.ReadAllText(filePath);
 
         var options = new SensitiveDataOptions { Getter = _ => "TestRedacted", Include = true };
-        var serializer = new SensitiveDataSerializer(Options.Create(options), NullLogger<SensitiveDataSerializer>.Instance, new SensitiveFieldsProvider());
+        var serializer = new SensitiveDataSerializer(Options.Create(options),
+            NullLogger<SensitiveDataSerializer>.Instance, new SensitiveFieldsProvider());
 
         var result = serializer.RedactRawJson(json, typeof(ImportNotification));
 
         JsonNode.DeepEquals(JsonNode.Parse(json), JsonNode.Parse(result)).Should().BeTrue();
-
     }
 
     [Fact]
     public void WhenIncludeSensitiveData_RedactedShouldBeDifferentJson()
     {
-        var filePath = "../../../Fixtures/SmokeTest/IPAFFS/CHEDA/CHEDA_GB_2024_1041389-ee0e6fcf-52a4-45ea-8830-d4553ee70361.json";
+        var filePath =
+            "../../../Fixtures/SmokeTest/IPAFFS/CHEDA/CHEDA_GB_2024_1041389-ee0e6fcf-52a4-45ea-8830-d4553ee70361.json";
         var json =
             File.ReadAllText(filePath);
 
         var options = new SensitiveDataOptions { Getter = _ => "TestRedacted", Include = false };
-        var serializer = new SensitiveDataSerializer(Options.Create(options), NullLogger<SensitiveDataSerializer>.Instance, new SensitiveFieldsProvider());
+        var serializer = new SensitiveDataSerializer(Options.Create(options),
+            NullLogger<SensitiveDataSerializer>.Instance, new SensitiveFieldsProvider());
 
         var result = serializer.RedactRawJson(json, typeof(ImportNotification));
 
         JsonNode.DeepEquals(JsonNode.Parse(json), JsonNode.Parse(result)).Should().BeFalse();
         result.Should().Contain("TestRedacted");
-
     }
 
     [Fact]
     public void WhenIncludeSensitiveData_DataShouldBeRedacted()
     {
-        var filePath = "../../../Fixtures/SmokeTest/IPAFFS/CHEDA/CHEDA_GB_2024_1041389-ee0e6fcf-52a4-45ea-8830-d4553ee70361.json";
+        var filePath =
+            "../../../Fixtures/SmokeTest/IPAFFS/CHEDA/CHEDA_GB_2024_1041389-ee0e6fcf-52a4-45ea-8830-d4553ee70361.json";
         var json =
             File.ReadAllText(filePath);
 
         var options = new SensitiveDataOptions { Getter = _ => "TestRedacted", Include = false };
-        var serializer = new SensitiveDataSerializer(Options.Create(options), NullLogger<SensitiveDataSerializer>.Instance, new SensitiveFieldsProvider());
+        var serializer = new SensitiveDataSerializer(Options.Create(options),
+            NullLogger<SensitiveDataSerializer>.Instance, new SensitiveFieldsProvider());
 
         var result = serializer.Deserialize<ImportNotification>(json);
 
-       
-        result.PartOne?.Consignee?.Address?.AddressLine1.Should().Contain("TestRedacted");
 
+        result.PartOne?.Consignee?.Address?.AddressLine1.Should().Contain("TestRedacted");
     }
 }
