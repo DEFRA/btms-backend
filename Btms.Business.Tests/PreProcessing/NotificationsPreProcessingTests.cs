5296fa100f7b0b9b60c8e63c4de9df8a6552c0b5
t11omas <t11omas1983@live.co.uk>
2025-02-19 10:53:05 +0000

    updated to not process in progress if the notification status is vali… (#148)
    * updated to not process in progress if the notification status is validated, or rejected
    
    * updated tests
    
    * skipped test as test data needs to be looked at
    
    ---------
    
    Co-authored-by: Thomas Anderson <thomas.anderson@esynergy.co.uk>

--- Btms.Business.Tests/PreProcessing/NotificationsPreProcessingTests.cs
+++ Btms.Business.Tests/PreProcessing/NotificationsPreProcessingTests.cs
@@ -55,14 +55,45 @@ public class NotificationsPreProcessingTests
         savedNotification?.Updated.Should().BeAfter(default);
     }
 
-    private static ImportNotification CreateImportNotification()
+    [Theory]
+    [InlineData(ImportNotificationStatusEnum.Validated, Model.Ipaffs.ImportNotificationStatusEnum.Validated)]
+    [InlineData(ImportNotificationStatusEnum.Rejected, Model.Ipaffs.ImportNotificationStatusEnum.Rejected)]
+    [InlineData(ImportNotificationStatusEnum.PartiallyRejected, Model.Ipaffs.ImportNotificationStatusEnum.PartiallyRejected)]
+
+    [InlineData(ImportNotificationStatusEnum.Submitted, Model.Ipaffs.ImportNotificationStatusEnum.InProgress)]
+    [InlineData(ImportNotificationStatusEnum.Amend, Model.Ipaffs.ImportNotificationStatusEnum.InProgress)]
+    [InlineData(ImportNotificationStatusEnum.Modify, Model.Ipaffs.ImportNotificationStatusEnum.InProgress)]
+    [InlineData(ImportNotificationStatusEnum.Replaced, Model.Ipaffs.ImportNotificationStatusEnum.InProgress)]
+    public async Task WhenNotificationExists_AndLastUpdatedIsNewer_AndStatusInGoingBackToInProgress_ThenShouldNotBeUpdated(ImportNotificationStatusEnum status, Model.Ipaffs.ImportNotificationStatusEnum expectedStatus)
+    {
+        // ARRANGE
+        var notification = CreateImportNotification(status);
+        var dbContext = new MemoryMongoDbContext();
+        await dbContext.Notifications.Insert(notification.MapWithTransform());
+        notification.LastUpdated = notification.LastUpdated?.AddHours(1);
+        notification.Status = ImportNotificationStatusEnum.InProgress;
+        var preProcessor = new ImportNotificationPreProcessor(dbContext, NullLogger<ImportNotificationPreProcessor>.Instance);
+
+        // ACT
+        var preProcessingResult = await preProcessor.Process(
+            new PreProcessingContext<ImportNotification>(notification, "TestMessageId"));
+
+        // ASSERT
+        preProcessingResult.Outcome.Should().Be((int)status == (int)expectedStatus ? PreProcessingOutcome.Skipped : PreProcessingOutcome.Changed);
+        var savedNotification = await dbContext.Notifications.Find(notification.ReferenceNumber!);
+        savedNotification?.Status.Should().Be(expectedStatus);
+        savedNotification.Should().NotBeNull();
+        savedNotification?.AuditEntries.Count.Should().Be(preProcessingResult.Outcome == PreProcessingOutcome.Skipped ? 0 : 1);
+    }
+
+    private static ImportNotification CreateImportNotification(ImportNotificationStatusEnum status = ImportNotificationStatusEnum.Submitted)
     {
         return ImportNotificationBuilder.Default()
             .WithReferenceNumber(ImportNotificationTypeEnum.Chedpp, 1, DateTime.UtcNow, 1)
             .WithRandomCommodities(1, 2)
             .Do(x =>
             {
-                x.Status = ImportNotificationStatusEnum.Submitted;
+                x.Status = status;
                 foreach (var parameterSet in x.PartOne?.Commodities?.ComplementParameterSets!)
                 {
                     parameterSet.KeyDataPairs = null;
