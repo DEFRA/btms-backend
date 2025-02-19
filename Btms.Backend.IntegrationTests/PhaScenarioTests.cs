4dee109774d75186aa795b5c2636fbb583abbfd6
d-ee-klemm <184640818+d-ee-klemm@users.noreply.github.com>
2025-02-19 20:18:56 +0000

    PHAIN-82 : PHA Test Scenarios (#142)
    * PHAIN-82: Add redacted data and tests to generate BTMS responses for PHA test scenarios
    
    * PHAIN-82: Add redacted data
    
    * PHAIN-82 : Fix data redaction of arrays
    :
    
    * PHAIN-82 : update stub data
    
    * PHAIN-82 : update stub data in include GMR related Import Notifications
    
    * PHAIN-82 : update stub data in include GMR related Import Notifications
    
    * PHAIN-82 : update stub data in include GMR related Import Notifications
    
    * PHAIN-82 : update stub data in include GMR related Import Notifications
    
    * PHAIN-82 : update stub data in include GMR related Import Notifications
    
    * PHAIN-82 : update stub data in include GMR related Import Notifications
    
    * PHAIN-82 : update stub data in include Finalisation Sample
    
    * PHAIN-82 : update stub data in include Finalisation Sample
    
    * PHAIN-82 : update stub data in include Finalisation Sample
    
    * PHAIN-82 : update stub data in include Finalisation Sample
    
    ---------
    
    Co-authored-by: Tim Peel <tim.peel@equalexperts.com>
    Co-authored-by: Tom McArdle <79710668+tmc-ee@users.noreply.github.com>

---
+++ Btms.Backend.IntegrationTests/PhaScenarioTests.cs
@@ -0,0 +1,116 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Btms.SensitiveData;
+using Btms.Types.Ipaffs;
+using FluentAssertions;
+using JsonApiDotNetCore.Serialization.Objects;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using TestDataGenerator.Scenarios.PhaStubs;
+using TestGenerator.IntegrationTesting.Backend;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Btms.Backend.IntegrationTests;
+
+public class PhaScenarioTests(ITestOutputHelper testOutputHelper) : MultipleScenarioGeneratorBaseTest(testOutputHelper)
+{
+    [Theory]
+    [InlineData(typeof(PhaStubScenarioGenerator), "PhaStub")]
+    [InlineData(typeof(PhaFinalisationStubScenarioGenerator), "PhaFinalisationStub")]
+    public async Task ShouldImportPhaStubScenario(Type generatorType, string folder)
+    {
+        var exportData = false;
+        var redactData = false;
+
+        if (redactData)
+            await RedactIPAFFSFiles(folder);
+
+        EnsureEnvironmentInitialised(generatorType);
+
+        var expectedImportNotifications = LoadedData
+            .Where(l => l.Message is ImportNotification)
+            .Select(l => (ImportNotification)l.Message);
+
+        var importedNotifications = Client.AsJsonApiClient().Get("api/import-notifications?page[size]=20");
+        var importedNotificationIds = importedNotifications.Data.Select(d => d.Id).ToArray();
+
+        foreach (var expectedImportNotification in expectedImportNotifications)
+        {
+            importedNotificationIds.Should().Contain(expectedImportNotification.ReferenceNumber);
+        }
+
+        if (exportData)
+            await ExportData(importedNotifications.Data);
+
+    }
+
+    private async Task ExportData(List<ResourceObject> importedNotifications)
+    {
+        const string outputDirectory = "../../../PhaScenarioTestsOutput";
+        Directory.CreateDirectory(outputDirectory);
+
+        var relatedMovements = new List<ResourceIdentifierObject>();
+        var relatedGoodsMovements = new List<ResourceIdentifierObject>();
+
+        foreach (var importNotification in importedNotifications)
+        {
+            var json = await GetDocument($"api/import-notifications/{importNotification.Id}", Client.AsHttpClient());
+
+            var jsonNode = JsonNode.Parse(json);
+            jsonNode!["data"]!["attributes"]!["partOne"]!["pointOfEntry"] = "GBTEEP1";
+            var jsonString = jsonNode.ToJsonString(new JsonSerializerOptions { WriteIndented = true });
+
+            await File.WriteAllTextAsync($"{outputDirectory}/btms-import-notification-single-{importNotification.Id}.json", jsonString);
+
+            if (importNotification.Relationships!.TryGetValue("movements", out var movements))
+                relatedMovements.AddRange(movements!.Data.ManyValue!);
+
+            if (importNotification.Relationships!.TryGetValue("gmrs", out var goodsMovements))
+                relatedGoodsMovements.AddRange(goodsMovements!.Data.ManyValue!);
+        }
+
+        var mrns = relatedMovements.Select(r => r.Id).Distinct();
+        foreach (var mrn in mrns)
+        {
+            var json = await GetDocument($"api/movements/{mrn}", Client.AsHttpClient());
+            await File.WriteAllTextAsync($"{outputDirectory}/btms-movement-single-{mrn}.json", json);
+        }
+
+        var grmIds = relatedGoodsMovements.Select(r => r.Id).Distinct();
+        foreach (var grmId in grmIds)
+        {
+            var json = await GetDocument($"api/gmrs/{grmId}", Client.AsHttpClient());
+            await File.WriteAllTextAsync($"{outputDirectory}/btms-goods-movement-single-{grmId}.json", json);
+        }
+    }
+
+    private async Task<string> GetDocument(string path, HttpClient httpClient)
+    {
+        var response = await Client.AsHttpClient().GetStringAsync(path);
+
+        var document = JsonDocument.Parse(response);
+#pragma warning disable CA1869
+        return JsonSerializer.Serialize(document, new JsonSerializerOptions { WriteIndented = true });
+#pragma warning restore CA1869
+    }
+
+    private async Task RedactIPAFFSFiles(string scenarioFolder)
+    {
+        var di = new DirectoryInfo($"../../../../TestDataGenerator/Scenarios/Samples/{scenarioFolder}/IPAFFS");
+
+        var files = di.GetFiles("*.json", SearchOption.AllDirectories);
+
+        await Parallel.ForEachAsync(files, async (fileInfo, ct) =>
+        {
+
+            var json = await File.ReadAllTextAsync(fileInfo.FullName, ct);
+            var options = new SensitiveDataOptions { Include = false };
+            var serializer =
+                new SensitiveDataSerializer(Options.Create(options), NullLogger<SensitiveDataSerializer>.Instance, new SensitiveFieldsProvider());
+            var result = serializer.RedactRawJson(json, typeof(ImportNotification));
+            await File.WriteAllTextAsync(fileInfo.FullName, result, ct);
+        });
+    }
+
+}
---
+++ TestDataGenerator/Scenarios/PhaStubs/PhaStubScenarioGenerator.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Logging;
+using TestDataGenerator.Scenarios.SpecificFiles;
+
+namespace TestDataGenerator.Scenarios.PhaStubs;
+
+
+public class PhaStubScenarioGenerator(IServiceProvider sp, ILogger<PhaStubScenarioGenerator> logger)
+    : SpecificFilesScenarioGenerator(sp, logger, "PhaStub")
+{
+
+}
+
+public class PhaFinalisationStubScenarioGenerator(IServiceProvider sp, ILogger<PhaStubScenarioGenerator> logger)
+    : SpecificFilesScenarioGenerator(sp, logger, "PhaFinalisationStub")
+{
+
+}
