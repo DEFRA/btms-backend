50cd530c47a1a38fea6c718c9769b9cfef4d59ea
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-02-20 13:47:37 +0000

    updated exception message

--- TestDataGenerator/Helpers/DataHelpers.cs
+++ TestDataGenerator/Helpers/DataHelpers.cs
@@ -90,7 +90,7 @@ public static class DataHelpers
     {
         var prefix = chedType.AsString();
 
-        if (item > 9999) throw new ArgumentException("Currently only deals with max 100,000 items");
+        if (item > 9999) throw new ArgumentException("Currently only deals with max 10,000 items");
 
         var identifier = $"{scenario:00}{created.DateRef()}{item + 1}";
         return $"{prefix}.GB.{created.Year}.{identifier}";
