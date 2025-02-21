7b366713d7a8b285d51befc532c91cf77381cf18
Ian Shimmings <ian.shimmings@hotmail.com>
2025-02-21 16:36:20 +0000

    Moved local config to local env file

--- Btms.Backend.IntegrationTests/Consumers/AmazonQueues/TestAwsConsumers.cs
+++ Btms.Backend.IntegrationTests/Consumers/AmazonQueues/TestAwsConsumers.cs
@@ -23,7 +23,7 @@ public class TestAwsConsumers : IAsyncDisposable
         var builder = WebApplication.CreateBuilder();
 
         builder.Configuration.AddJsonFile("appsettings.json", optional: false)
-            .AddJsonFile("appsettings.Development.json", optional: true)
+            .AddIniFile("Properties/local.env", true)
             .AddEnvironmentVariables();
 
         builder.Services.AddScoped<IClearanceRequestConsumer>(_ => ClearanceRequestConsumer.Mock);
