025ecca5b7275cf7b0d22370fc7207593c3d082f
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-02-25 08:08:02 +0000

    removed duplicate code

--- Btms.Business/Commands/InitialiseCommand.cs
+++ Btms.Business/Commands/InitialiseCommand.cs
@@ -23,85 +23,57 @@ public class InitialiseCommand : SyncCommand
         IBtmsMediator mediator,
         ISyncJobStore syncJobStore,
         IFeatureManager featureManager)
-        : Handler<InitialiseCommand>(syncMetrics, bus, logger, sensitiveDataSerializer, blobService, businessOptions, syncJobStore)
+        : Handler<InitialiseCommand>(syncMetrics, bus, logger, sensitiveDataSerializer, blobService, businessOptions,
+            syncJobStore)
     {
         public override async Task Handle(InitialiseCommand request, CancellationToken cancellationToken)
         {
+            var job = SyncJobStore.GetJob(request.JobId)!;
+            job.Start();
+
+            SyncNotificationsCommand notifications = new() { SyncPeriod = request.SyncPeriod };
+            await mediator.SendSyncJob(notifications, cancellationToken);
+
+            Logger.LogInformation("Started Notifications {0} sync job. Waiting on Notifications job to complete.",
+                notifications.JobId);
+
             if (await featureManager.IsEnabledAsync(Features.SyncPerformanceEnhancements))
             {
-                await Sequential(request, cancellationToken);
+                await SyncJobStore.WaitOnJobCompleting(notifications.JobId);
             }
-            else
-            {
-                await Parallel(request, cancellationToken);
-            }
-        }
-
-        private async Task Parallel(InitialiseCommand request, CancellationToken cancellationToken)
-        {
-            var job = SyncJobStore.GetJob(request.JobId)!;
 
             SyncClearanceRequestsCommand clearanceRequests = new() { SyncPeriod = request.SyncPeriod };
             await mediator.SendSyncJob(clearanceRequests, cancellationToken);
 
-            SyncNotificationsCommand notifications = new() { SyncPeriod = request.SyncPeriod };
-            await mediator.SendSyncJob(notifications, cancellationToken);
-
-            Logger.LogInformation("Started Notifications {0} and ClearanceRequests {1} sync jobs. Waiting on ClearanceRequests job to complete.", notifications.JobId, clearanceRequests.JobId);
-
-            job.Start();
+            Logger.LogInformation(
+                "Started ClearanceRequests {0} sync jobs. Waiting on ClearanceRequests job to complete.",
+                clearanceRequests.JobId);
 
             await SyncJobStore.WaitOnJobCompleting(clearanceRequests.JobId);
 
+            var decisions = RunDecisionsAndFinalisations(request, cancellationToken);
+
+            await Task.WhenAll(
+                decisions,
+                SyncJobStore.WaitOnJobCompleting(notifications.JobId));
+
+            job.Complete();
+        }
+
+        private async Task RunDecisionsAndFinalisations(InitialiseCommand request, CancellationToken cancellationToken)
+        {
             SyncDecisionsCommand decisions = new() { SyncPeriod = request.SyncPeriod };
             await mediator.SendSyncJob(decisions, cancellationToken);
 
             SyncFinalisationsCommand finalisations = new() { SyncPeriod = request.SyncPeriod };
             await mediator.SendSyncJob(finalisations, cancellationToken);
 
-            Logger.LogInformation("ClearanceRequests sync job complete. Started Decisions {0} and Finalisations {1} sync jobs", decisions.JobId, finalisations.JobId);
+            Logger.LogInformation("Started Decisions {0} and Finalisations {1} sync jobs", decisions.JobId,
+                finalisations.JobId);
 
             await Task.WhenAll(
                 SyncJobStore.WaitOnJobCompleting(decisions.JobId),
-                SyncJobStore.WaitOnJobCompleting(finalisations.JobId),
-                SyncJobStore.WaitOnJobCompleting(notifications.JobId));
-
-            job.Complete();
-        }
-
-        private async Task Sequential(InitialiseCommand request, CancellationToken cancellationToken)
-        {
-            var job = SyncJobStore.GetJob(request.JobId)!;
-            job.Start();
-
-            SyncNotificationsCommand notifications = new() { SyncPeriod = request.SyncPeriod };
-            await mediator.SendSyncJob(notifications, cancellationToken);
-
-            Logger.LogInformation("Started Notifications {0} sync job. Waiting on Notifications job to complete.", notifications.JobId);
-
-            await SyncJobStore.WaitOnJobCompleting(notifications.JobId);
-
-            SyncClearanceRequestsCommand clearanceRequests = new() { SyncPeriod = request.SyncPeriod };
-            await mediator.SendSyncJob(clearanceRequests, cancellationToken);
-
-            Logger.LogInformation("Started ClearanceRequests {0} sync jobs. Waiting on ClearanceRequests job to complete.", clearanceRequests.JobId);
-
-            await SyncJobStore.WaitOnJobCompleting(clearanceRequests.JobId);
-
-            SyncDecisionsCommand decisions = new() { SyncPeriod = request.SyncPeriod };
-            await mediator.SendSyncJob(decisions, cancellationToken);
-
-            SyncFinalisationsCommand finalisations = new() { SyncPeriod = request.SyncPeriod };
-            await mediator.SendSyncJob(finalisations, cancellationToken);
-
-            Logger.LogInformation("ClearanceRequests sync job complete. Started Decisions {0} and Finalisations {1} sync jobs", decisions.JobId, finalisations.JobId);
-
-            await Task.WhenAll(
-                SyncJobStore.WaitOnJobCompleting(decisions.JobId),
-                SyncJobStore.WaitOnJobCompleting(finalisations.JobId),
-                SyncJobStore.WaitOnJobCompleting(notifications.JobId));
-
-            job.Complete();
+                SyncJobStore.WaitOnJobCompleting(finalisations.JobId));
         }
     }
 
