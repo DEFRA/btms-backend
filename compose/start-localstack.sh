95690de3cfa2ff9b9050ccf0c96ea0508518202e
Ian Shimmings <ian.shimmings@hotmail.com>
2025-02-26 14:10:38 +0000

    Moved localstack into Dockerfile

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -2,11 +2,16 @@
 
 ENDPOINT_URL=http://sqs.eu-west-2.localhost.localstack.cloud:4566
 
-export AWS_ENDPOINT_URL=$ENDPOINT_URL
+export AWS_ENDPOINT_URL=ENDPOINT_URL
 export AWS_REGION=eu-west-2
 export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=local
 export AWS_SECRET_ACCESS_KEY=local
+
+# S3 buckets
+# aws --endpoint-url=http://localhost:4966 s3 mb s3://my-bucket
+
+# SNS/SQS topics, queues, subscriptions
 
 CCR=customs_clearance_request.fifo
 CFN=customs_finalisation_notification.fifo
