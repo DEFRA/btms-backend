59a41580849fd8e1c252c7489586f80d455f1d72
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-02-27 08:59:08 +0000

    added sqs health check

---
+++ Btms.Backend/Aws/BtmsSqsHealthCheckBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Btms.Consumers.AmazonQueues;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Btms.Backend.Aws;
+
+public static class BtmsSqsHealthCheckBuilderExtensions
+{
+    private const string NAME = "aws sqs";
+
+    public static IHealthChecksBuilder AddBtmsSqs(
+        this IHealthChecksBuilder builder,
+        AwsSqsOptions options,
+        string? name = default,
+        HealthStatus? failureStatus = default,
+        IEnumerable<string>? tags = default,
+        TimeSpan? timeout = default)
+    {
+        return builder.Add(new HealthCheckRegistration(
+            name ?? NAME,
+            sp => new BtmsSqsHealthCheck(options),
+            failureStatus,
+            tags,
+            timeout));
+    }
+}
