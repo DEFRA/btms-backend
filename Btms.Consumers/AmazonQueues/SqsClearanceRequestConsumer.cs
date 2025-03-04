18009e99189e2b5e90a1157a2064febbd10f4260
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-03-04 10:54:51 +0000

    added logging

--- Btms.Consumers/AmazonQueues/SqsClearanceRequestConsumer.cs
+++ Btms.Consumers/AmazonQueues/SqsClearanceRequestConsumer.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Btms.Backend.Data;
 using Btms.Business.Pipelines.PreProcessing;
 using Btms.Business.Services.Decisions;
@@ -7,6 +8,7 @@ using Btms.Business.Services.Validating;
 using Btms.Types.Alvs;
 using Microsoft.Extensions.Logging;
 using SlimMessageBus;
+using SlimMessageBus.Host.AmazonSQS;
 
 namespace Btms.Consumers.AmazonQueues;
 
@@ -22,14 +24,24 @@ internal class SqsClearanceRequestConsumer(
 {
     public Task OnHandle(MessageBody message, CancellationToken cancellationToken)
     {
-        var innerConsumer = new ClearanceRequestConsumer(preProcessor, linkingService, matchingService, decisionService,
-            validationService, dbContext, logger);
+        try
+        {
+            var innerConsumer = new ClearanceRequestConsumer(preProcessor, linkingService, matchingService, decisionService,
+                validationService, dbContext, logger);
 
-        innerConsumer.Context = Context;
+            innerConsumer.Context = Context;
 
-        var clearanceRequest = message.MessageAs<AlvsClearanceRequest>() ?? throw new InvalidOperationException();
+            var clearanceRequest = message.MessageAs<AlvsClearanceRequest>() ?? throw new InvalidOperationException();
 
-        return innerConsumer.OnHandle(clearanceRequest, cancellationToken);
+            return innerConsumer.OnHandle(clearanceRequest, cancellationToken);
+        }
+        catch (Exception)
+        {
+            var tp = Context.GetTransportMessage();
+            logger.LogError("Failed to process SQS Message {RawMessage}", JsonSerializer.Serialize(tp));
+            throw;
+        }
+      
     }
 
     public IConsumerContext Context { get; set; } = null!;
