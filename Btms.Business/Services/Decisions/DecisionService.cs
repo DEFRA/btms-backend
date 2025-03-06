097d27f8b5859bac7af685fcf56544802fec65f4
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-03-06 06:01:50 +0000

    formatting

--- Btms.Business/Services/Decisions/DecisionService.cs
+++ Btms.Business/Services/Decisions/DecisionService.cs
@@ -187,7 +187,6 @@ public class DecisionService(
                     results.AddRange(GetDecisionsForCheckCode(notification, checkCode, finders));
                 }
             }
-            
         }
 
         var item = 1;
