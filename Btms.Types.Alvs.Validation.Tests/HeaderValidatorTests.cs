56d1723a54c4a84ab957b2d7c8eacf5e01eac97e
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-03-20 07:42:37 +0000

    refactored items and header tests

--- Btms.Types.Alvs.Validation.Tests/HeaderValidatorTests.cs
+++ Btms.Types.Alvs.Validation.Tests/HeaderValidatorTests.cs
@@ -7,101 +7,58 @@ public class HeaderValidatorTests
 {
     private HeaderValidator validator = new("123");
 
-    [Fact]
-    public void Should_have_error_when_EntryReference_is_null()
+    [Theory]
+    [ClassData(typeof(HeaderValidatorTestData))]
+    public void TheoryTests(Header model, ExpectedResult expectedResult)
     {
-        var model = new Header { EntryReference = null };
         var result = validator.TestValidate(model);
-        result.ShouldHaveValidationErrorFor(p => p.EntryReference);
+
+        if (expectedResult.HasValidationError)
+        {
+            result.ShouldHaveValidationErrorFor(expectedResult.PropertyName);
+        }
+        else
+        {
+            result.ShouldNotHaveValidationErrorFor(expectedResult.PropertyName);
+        }
     }
 
-    [Fact]
-    public void Should_not_have_error_when_EntryReference_is_specified()
+    public class HeaderValidatorTestData : TheoryData<Header, ExpectedResult>
     {
-        var model = new Header { EntryReference = "test" };
-        var result = validator.TestValidate(model);
-        result.ShouldNotHaveValidationErrorFor(p => p.EntryReference);
+        public HeaderValidatorTestData()
+        {
+            Add(new Header { EntryReference = "15GB1245fst7s8g9s4" }, new ExpectedResult(nameof(Header.EntryReference), false));
+            Add(new Header { EntryReference = "invalid" }, new ExpectedResult(nameof(Header.EntryReference), true));
+            Add(new Header { EntryReference = null }, new ExpectedResult(nameof(Header.EntryReference), true));
+
+            Add(new Header { EntryVersionNumber = 2 }, new ExpectedResult(nameof(Header.EntryVersionNumber), false));
+            Add(new Header { EntryVersionNumber = -1 }, new ExpectedResult(nameof(Header.EntryVersionNumber), true));
+            Add(new Header { EntryVersionNumber = 100 }, new ExpectedResult(nameof(Header.EntryVersionNumber), true));
+            Add(new Header { EntryVersionNumber = null }, new ExpectedResult(nameof(Header.EntryVersionNumber), true));
+
+            Add(new Header { EntryVersionNumber = 2, PreviousVersionNumber = 1 }, new ExpectedResult(nameof(Header.EntryVersionNumber), false));
+            Add(new Header { EntryVersionNumber = 1, PreviousVersionNumber = 2 }, new ExpectedResult(nameof(Header.EntryVersionNumber), false));
+
+            Add(new Header { DeclarationUcr = "Valid" }, new ExpectedResult(nameof(Header.DeclarationUcr), false));
+            Add(new Header { DeclarationUcr = "1234567891234567891233fghytfcdsertgy" }, new ExpectedResult(nameof(Header.DeclarationUcr), true));
+            Add(new Header { DeclarationUcr = null }, new ExpectedResult(nameof(Header.DeclarationUcr), true));
+
+            Add(new Header { DeclarationType = "F" }, new ExpectedResult(nameof(Header.DeclarationType), false));
+            Add(new Header { DeclarationType = "T" }, new ExpectedResult(nameof(Header.DeclarationType), true));
+            Add(new Header { DeclarationType = null }, new ExpectedResult(nameof(Header.DeclarationType), true));
+
+            Add(new Header { DeclarantId = "valid" }, new ExpectedResult(nameof(Header.DeclarantId), false));
+            Add(new Header { DeclarantId = "1234567891234567891233fghytfcdsertgy" }, new ExpectedResult(nameof(Header.DeclarantId), true));
+            Add(new Header { DeclarantId = null }, new ExpectedResult(nameof(Header.DeclarantId), true));
+
+            Add(new Header { DeclarantName = "valid" }, new ExpectedResult(nameof(Header.DeclarantName), false));
+            Add(new Header { DeclarantName = "1234567891234567891233fghytfcdsertgy" }, new ExpectedResult(nameof(Header.DeclarantName), true));
+            Add(new Header { DeclarantName = null }, new ExpectedResult(nameof(Header.DeclarantName), true));
+
+            Add(new Header { DispatchCountryCode = "GB" }, new ExpectedResult(nameof(Header.DispatchCountryCode), false));
+            Add(new Header { DispatchCountryCode = "T" }, new ExpectedResult(nameof(Header.DispatchCountryCode), true));
+            Add(new Header { DispatchCountryCode = "GBB" }, new ExpectedResult(nameof(Header.DispatchCountryCode), true));
+            Add(new Header { DispatchCountryCode = null }, new ExpectedResult(nameof(Header.DispatchCountryCode), true));
+        }
     }
-
-    [Fact]
-    public void Should_have_error_when_EntryVersionNumber_is_null()
-    {
-        var model = new Header { EntryVersionNumber = null };
-        var result = validator.TestValidate(model);
-        result.ShouldHaveValidationErrorFor(p => p.EntryVersionNumber);
-    }
-
-    [Fact]
-    public void Should_not_have_error_when_EntryVersionNumber_is_specified()
-    {
-        var model = new Header { EntryVersionNumber = 1 };
-        var result = validator.TestValidate(model);
-        result.ShouldNotHaveValidationErrorFor(p => p.EntryVersionNumber);
-    }
-
-    [Fact]
-    public void Should_have_error_when_DeclarationUcr_is_null()
-    {
-        var model = new Header { DeclarationUcr = null };
-        var result = validator.TestValidate(model);
-        result.ShouldHaveValidationErrorFor(p => p.DeclarationUcr);
-    }
-
-    [Fact]
-    public void Should_not_have_error_when_DeclarationUcr_is_specified()
-    {
-        var model = new Header { DeclarationUcr = "test" };
-        var result = validator.TestValidate(model);
-        result.ShouldNotHaveValidationErrorFor(p => p.DeclarationUcr);
-    }
-
-    [Fact]
-    public void Should_have_error_when_DeclarantId_is_null()
-    {
-        var model = new Header { DeclarantId = null };
-        var result = validator.TestValidate(model);
-        result.ShouldHaveValidationErrorFor(p => p.DeclarantId);
-    }
-
-    [Fact]
-    public void Should_not_have_error_when_DeclarantId_is_specified()
-    {
-        var model = new Header { DeclarantId = "test" };
-        var result = validator.TestValidate(model);
-        result.ShouldNotHaveValidationErrorFor(p => p.DeclarantId);
-    }
-
-    [Fact]
-    public void Should_have_error_when_DeclarantName_is_null()
-    {
-        var model = new Header { DeclarantName = null };
-        var result = validator.TestValidate(model);
-        result.ShouldHaveValidationErrorFor(p => p.DeclarantName);
-    }
-
-    [Fact]
-    public void Should_not_have_error_when_DeclarantName_is_specified()
-    {
-        var model = new Header { DeclarantName = "test" };
-        var result = validator.TestValidate(model);
-        result.ShouldNotHaveValidationErrorFor(p => p.DeclarantName);
-    }
-
-    [Fact]
-    public void Should_have_error_when_DispatchCountryCode_is_null()
-    {
-        var model = new Header { DispatchCountryCode = null };
-        var result = validator.TestValidate(model);
-        result.ShouldHaveValidationErrorFor(p => p.DispatchCountryCode);
-    }
-
-    [Fact]
-    public void Should_not_have_error_when_DispatchCountryCode_is_specified()
-    {
-        var model = new Header { DispatchCountryCode = "GB" };
-        var result = validator.TestValidate(model);
-        result.ShouldNotHaveValidationErrorFor(p => p.DispatchCountryCode);
-    }
-
-
 }
