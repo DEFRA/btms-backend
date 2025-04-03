577f364d8b82d397d1c9a2ead611ef8d795b6a1e
Thomas Anderson <thomas.anderson@esynergy.co.uk>
2025-04-03 04:42:51 +0100

    added some extra logging

--- Btms.Consumers/Interceptors/ConsumerContextAccessorInterceptor.cs
+++ Btms.Consumers/Interceptors/ConsumerContextAccessorInterceptor.cs
@@ -1,12 +1,14 @@
 using Btms.Consumers.ConsumerContextAccessor.ContextAccessor;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Serilog.Context;
 using SlimMessageBus;
 using SlimMessageBus.Host.Interceptor;
+using System.Collections.Generic;
 
 namespace Btms.Consumers.Interceptors;
 
-public class ConsumerContextAccessorInterceptor<TMessage>(IConfiguration configuration) : IConsumerInterceptor<TMessage>
+public class ConsumerContextAccessorInterceptor<TMessage>(IConfiguration configuration, ILogger<ConsumerContextAccessorInterceptor<TMessage>> logger) : IConsumerInterceptor<TMessage>
 {
     public async Task<object> OnHandle(TMessage message, Func<Task<object>> next, IConsumerContext context)
     {
@@ -26,9 +28,12 @@ public class ConsumerContextAccessorInterceptor<TMessage>(IConfiguration configu
         {
             using (LogContext.PushProperty("CorrelationId", requestHeader))
             {
+                logger.LogInformation("Set CorrelationId to {CorrelationId}", requestHeader);
                 return await next();
             }
         }
+
+        logger.LogInformation("No CorrelationId found on Message Header to {Headers}", string.Join(Environment.NewLine, context.Headers));
 
         return await next();
     }
